bf66c81b3971ca1f0a4125c867e359de9d4f510c
Mohamed Amgad Tageldin <mtageld@emory.edu>
2019-11-09 02:35:45 -0500

    Update download_crowdsource_dataset.py

--- download_crowdsource_dataset.py
+++ download_crowdsource_dataset.py
@@ -1,187 +1,208 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  6 13:25:37 2019
-
-@author: tageldim
-"""
 
 import os
 import sys
-CWD = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, CWD)
 import logging
-logger = logging.getLogger()
 import json
 import girder_client
 import datetime
-import wget
-from io import BytesIO
-from PIL import Image
-Image.MAX_IMAGE_PIXELS = 1000000000
 import csv
+import requests
+import numpy as np
+from skimage.transform import resize
+from imageio import imwrite
 
-# %%==========================================================
-# Params
-# ============================================================
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, CWD)
 
-APIURL = 'http://demo.kitware.com/histomicstk/api/v1/'
-source_folder_id = '5bbdeba3e629140048d017bb'
+from utils import (
+    create_directory_structure, printNlog, get_image_from_htk_response, cf)
 
-SAVEPATH = CWD
-
-# %%===========================================================================
-# Methods
 # =============================================================================
 
-def create_directory_structure(folderList):
-    """create folders if non-existent"""
-    savepaths = {'base': SAVEPATH}
-    for folder in folderList:
-        folderpath = os.path.join(SAVEPATH, folder)
-        savepaths[folder] = folderpath
-        try:
-            os.mkdir(folderpath)
-        except FileExistsError:
-            pass
-    return savepaths
 
-def printNlog(msg, level='info'):
-  print(msg)
-  if level == 'info': 
-    logger.info(msg)
-  elif level == 'error': 
-    logger.error(msg)
-  else:
-    pass
+def run_prepwork():
+    """Ground work"""
+    # create directories
+    savepaths = create_directory_structure(
+        ['wsis', 'annotations', 'masks', 'images', 'logs'])
 
-def get_image_from_htk_response(resp):
-    """Given a girder response, get np array image"""        
-    image_content = BytesIO(resp.content)
-    image_content.seek(0)
-    Image.open(image_content)
-    image = Image.open(image_content)
-    return image
+    # configure logger
+    now = str(datetime.datetime.now()).replace(' ', '_').replace(':', '_')
+    logging.basicConfig(
+        filename=os.path.join(savepaths['logs'], now + '.log'),
+        format='%(asctime)s %(levelname)-3s %(message)s', level=logging.INFO)
+    printNlog("STARTED.")
 
-# %%===========================================================================
-# Ground work
-# =============================================================================
+    # Connect to API
+    gc = girder_client.GirderClient(apiUrl=cf.APIURL)
+    if cf.apiKey is not None:
+        gc.authenticate(apiKey=cf.apiKey)
+    else:
+        printNlog("""Enter login information below.
+        If you don't have an account, register for free at:
+        http://demo.kitware.com/histomicstk/histomicstk
+        It only takes a minute.
+        """)
+        gc.authenticate(interactive=True)
 
-# create directories
-savepaths = create_directory_structure(
-        ['wsis','annotations','masks','images', 'logs'])
+    # Get list of slides to download
+    resp = gc.get("item?folderId=%s&limit=1000000" % cf.source_folder_id)
+    slides = dict()
+    for s in resp:
+        short_name = s['name'][:12]
+        slides[short_name] = {'name': s['name'], '_id': s['_id']}
+    del resp
 
-# configure logger
-now = str(datetime.datetime.now()).replace(' ', '_').replace(':', '_')
-logging.basicConfig(
-    filename=os.path.join(savepaths['logs'], now +'.log'), 
-    format='%(asctime)s %(levelname)-3s %(message)s', level=logging.INFO)
-printNlog("STARTED.")
-
-# Connect to API
-#printNlog("""Enter login information below. 
-#If you don't have an account, register for free at:
-#http://demo.kitware.com/histomicstk/histomicstk
-#It only takes a minute.
-#""")
-gc = girder_client.GirderClient(apiUrl = APIURL)
-# gc.authenticate(interactive=True)
-gc.authenticate(apiKey='GDetOb3GVvZfMImjPf8lf1yya4dbcaTfDfq9o62v')
-
-# Get list of slides to download
-resp = gc.get("item?folderId=%s&limit=1000000" % source_folder_id)
-slides = dict()
-for s in resp:   
-    short_name = s['name'][:12]
-    slides[short_name] = {'name': s['name'], '_id': s['_id']}
-del resp
-
-# Do not re-download slides 
-existing_slides = set([
+    # Do not re-download slides
+    existing_slides = set([
         j for j in os.listdir(savepaths['wsis']) if j.endswith('.svs')])
-slide_list = list(set(slides.keys()) - existing_slides)
+    slide_list = list(set(slides.keys()) - existing_slides)
 
-# %%===========================================================================
-# Create WSI download shell script
+    # restrict slide list if needed
+    if cf.SLIDES_TO_KEEP is not None:
+        slide_list = [j for j in slide_list if j in cf.SLIDES_TO_KEEP]
+
+    # Assign as configurations
+    cf.gc = gc
+    cf.savepaths = savepaths
+    cf.slide_list = slide_list
+    cf.slides = slides
+
 # =============================================================================
 
-# bat script if windows, else bash script
-ext = '.bat' if os.name == 'nt' else '.sh'
-savepaths['wsi_script'] = os.path.join(
-    savepaths['wsis'], 'download_wsis' + ext)
 
-# Add commands to batch or bash script
-printNlog("Adding download commands to %s" % savepaths['wsi_script'])
-resp = gc.post('api_key')
-apikey = resp['key']
-for slide_name in slide_list:
-    command_base = "girder-client --api-url %s --api-key %s download %s %s" % (
-            APIURL, apikey, slides[slide_name]['_id'], savepaths['wsis'])
-    with open(savepaths['wsi_script'], 'a') as f:
-        f.write(command_base + "\n")
-    
-# %%===========================================================================
-# Download JSON annotations
+def download_rgbs_and_masks():
+    """Download RGB images and corresponding masks"""
+    n_slides = len(cf.slide_list)
+
+    sldid = 0
+    with open(cf.ROIBOUNDSPATH, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+
+            slide_name = row[''][:12]
+            if slide_name not in cf.slide_list:
+                continue
+
+            sldid += 1
+            xmin = int(float(row['xmin']))
+            ymin = int(float(row['ymin']))
+
+            if 'images' in cf.PIPELINE:
+                printNlog("Downloading RGB image: Slide %d of %d (%s)" % (
+                    sldid, n_slides, slide_name))
+
+                # Default is to get things at base magnification
+                getStr = \
+                    "/item/%s/tiles/region?left=%d&right=%d&top=%d&bottom=%d" \
+                    % (
+                        cf.slides[slide_name]['_id'],
+                        xmin, int(float(row['xmax'])),
+                        ymin, int(float(row['ymax'])),)
+
+                if cf.MPP is not None:  # Specific microns-per-pixel
+                    mm = 0.001 * cf.MPP
+                    getStr += "&mm_x=%.4f&mm_y=%.4f" % (mm, mm)
+
+                elif cf.MAG is not None:  # Specific magnification
+                    getStr += "&magnification=%.2f" % cf.MAG
+
+                resp = cf.gc.get(getStr, jsonResp=False)
+                rgb = get_image_from_htk_response(resp)
+                rgb.save(
+                    os.path.join(
+                        cf.savepaths['images'], "%s_xmin%d_ymin%d_.png"
+                        % (row[''], xmin, ymin)))
+
+            if 'masks' in cf.PIPELINE:
+                printNlog("Downloading mask: Slide %d of %d (%s)" % (
+                    sldid, n_slides, slide_name))
+
+                mask = np.uint8(get_image_from_htk_response(
+                    requests.get(row['mask_link'])))
+
+                # resize to match RGB magnification
+                if 'images' in cf.PIPELINE:
+                    mask = np.uint8(resize(
+                        mask, output_shape=np.array(rgb).shape[:2],
+                        order=0, preserve_range=True))
+
+                imwrite(im=mask, uri=os.path.join(
+                    cf.savepaths['masks'], "%s_xmin%d_ymin%d_.png"
+                    % (row[''], xmin, ymin)))
+
 # =============================================================================
 
-for sldid, slide_name in enumerate(slide_list):
-    
-    printNlog("Downloading annotations: Slide %d of %d (%s)" % (
-                sldid+1, len(slide_list), slide_name))
-    annotations = gc.get("/annotation/item/" + slides[slide_name]['_id'])
-    savename = os.path.join(
-            savepaths['annotations'], slides[slide_name]['name'])
-    savename = savename.replace('.svs', '.json')
-    with open(savename, 'w') as f:
-        json.dump(annotations, f)
 
-# %%===========================================================================
-# Download all masks
+def download_jsons():
+    """Download JSON annotations -- at base magnification."""
+    for sldid, slide_name in enumerate(cf.slide_list):
+        printNlog("Downloading annotations (base mag): Slide %d of %d (%s)" % (
+            sldid + 1, len(cf.slide_list), slide_name))
+        annotations = cf.gc.get(
+            "/annotation/item/" + cf.slides[slide_name]['_id'])
+        savename = os.path.join(
+            cf.savepaths['annotations'], cf.slides[slide_name]['name'])
+        savename = savename.replace('.svs', '.json')
+        with open(savename, 'w') as f:
+            json.dump(annotations, f)
+
 # =============================================================================
 
-printNlog("Downloading masks to %s" % savepaths['masks'])
-mask_link = 'https://ndownloader.figshare.com/articles/7193138/versions/1'
-wget.download(mask_link, savepaths['masks'])
 
-# %%===========================================================================
-# Download RGB images
+def create_wsi_download_script():
+    """Create WSI download shell script"""
+    # bat script if windows, else bash script
+    ext = '.bat' if os.name == 'nt' else '.sh'
+    cf.savepaths['wsi_script'] = os.path.join(
+        cf.savepaths['wsis'], 'download_wsis' + ext)
+
+    # Add commands to batch or bash script
+    printNlog("Adding download commands to %s" % cf.savepaths['wsi_script'])
+    resp = cf.gc.post('api_key')
+    apikey = resp['key']
+    for slide_name in cf.slide_list:
+        command_base = \
+            "girder-client --api-url %s --api-key %s download %s %s" % (
+            cf.APIURL, apikey, cf.slides[slide_name]['_id'],
+            cf.savepaths['wsis'])
+        with open(cf.savepaths['wsi_script'], 'a') as f:
+            f.write(command_base + "\n")
+
 # =============================================================================
 
-sldid = 0
-with open('./roiBounds.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)    
-    for row in reader:
-        
-        sldid += 1
-        slide_name = row[''][:12]
-        
-        printNlog("Downloading RGB image: Slide %d of %d (%s)" % (
-                sldid+1, len(slide_list), slide_name))
-        
-        xmin = int(float(row['xmin'])) 
-        ymin = int(float(row['ymin']))
-        # Get RGB from whol slide
-        getStr = "/item/%s/tiles/region?left=%d&right=%d&top=%d&bottom=%d" % (
-          slides[slide_name]['_id'], 
-          xmin, int(float(row['xmax'])), ymin, int(float(row['ymax'])))
-        resp  = gc.get(getStr, jsonResp=False)
-        rgb = get_image_from_htk_response(resp)
-        # save RGB
-        xmin = int(float(row['xmin'])) 
-        ymin = int(float(row['ymin']))
-        rgb.save(os.path.join(savepaths['images'], "%s_xmin%d_ymin%d_.png" % (
-                row[''], xmin, ymin)))
-        
-# %%===========================================================================
-# Done
+
+def main():
+    """main pipeline."""
+    # add configs
+    run_prepwork()
+
+    # run pipeline
+    if ('images' in cf.PIPELINE) or ('masks' in cf.PIPELINE):
+        download_rgbs_and_masks()
+
+    if 'annotations' in cf.PIPELINE:
+        download_jsons()
+
+    if 'wsis' in cf.PIPELINE:
+        create_wsi_download_script()
+
+    printNlog("DONE.")
+    if 'wsis' in cf.PIPELINE:
+        printNlog("""
+            Now please run the batch or shell script from the command line
+            using the following command:
+            Windows:
+                %s
+            Mac or Linux:
+                bash %s
+            to download the full whole-slide images.
+            """ % (cf.savepaths['wsi_script'], cf.savepaths['wsi_script']))
+
+
 # =============================================================================
-        
-printNlog("DONE.")
-printNlog("""
-Now please run the batch or shell script from the command line using the 
-following command:
-Windows:
-    %s
-Mac or Linux:
-    bash %s
-to download the full whole-slide images.
-""" % (savepaths['wsi_script'], savepaths['wsi_script']))
+
+if __name__ == '__main__':
+    main()
